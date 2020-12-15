601ebd9c168fe3f21ad79e87a9936e5d34830240
Thomas Decrick <thomas.decrick@icloud.com>
2020-12-15 23:01:15 +0100

    Counting Core Data results: Done

--- TD Portfolio/AwardsView.swift
+++ TD Portfolio/AwardsView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct AwardsView: View {
+    
     static let tag: String? = "Awards"
+    
+    @EnvironmentObject var dataController: DataController
+    
+    @State private var selectedAward = Award.example
+    @State private var showingAlertDetails = false
     
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
@@ -20,20 +26,29 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            //no action yet
+                            selectedAward = award
+                            showingAlertDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(Color.secondary.opacity(0.5))
+                                .foregroundColor( dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5) )
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
         }
+        .alert(isPresented: $showingAlertDetails, content: { () -> Alert in
+            if dataController.hasEarned(award: selectedAward) {
+                return Alert(title: Text("Unlocked \(selectedAward.name)"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+            } else {
+                return Alert(title: Text("Locked"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+            }
+            
+        })
     }
 }
 
