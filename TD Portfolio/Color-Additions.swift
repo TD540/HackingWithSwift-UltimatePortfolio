36614dec83bb09d3f7dc0bf6e231afcfb97ba612
Thomas Decrick <thomas.decrick@icloud.com>
2020-12-22 20:22:35 +0100

    Creating the Home view: Done

---
+++ TD Portfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  TD Portfolio
+//
+//  Created by Thomas Decrick on 19/12/2020.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
