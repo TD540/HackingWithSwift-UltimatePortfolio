e1544a6758bdf3eee73a402cb58dc2bb8551af1e
Thomas Decrick <thomas.decrick@icloud.com>
2020-12-25 20:18:01 +0100

    Core Data delete rules and predicates

--- TD Portfolio/HomeView.swift
+++ TD Portfolio/HomeView.swift
@@ -23,10 +23,15 @@ struct HomeView: View {
     
     init() {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "completed = false")
+        let completedPredicate = NSPredicate(format: "completed = false")
+        let openPredicate = NSPredicate(format: "project.closed = false")
+        let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [completedPredicate, openPredicate])
+        request.predicate = compoundPredicate
+        
         request.sortDescriptors = [
             NSSortDescriptor(keyPath: \Item.priority, ascending: false)
         ]
+        
         request.fetchLimit = 10
         items = FetchRequest(fetchRequest: request)
     }
@@ -52,6 +57,12 @@ struct HomeView: View {
             }
             .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
+            .toolbar {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
         }
     }
 }
