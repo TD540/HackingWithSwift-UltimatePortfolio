1b70cec78b0529363e1c38656b79f1c1c2af19d9
Thomas Decrick <thomas.decrick@icloud.com>
2021-01-18 19:49:28 +0100

    Testing

---
+++ TDPortfolioTests/AssetTest.swift
@@ -0,0 +1,21 @@
+//
+//  AssetTest.swift
+//  TDPortfolioTests
+//
+//  Created by Thomas Decrick on 13/01/2021.
+//
+
+import XCTest
+@testable import TDPortfolio
+
+class AssetTest: XCTestCase {
+    func testColorsExist() {
+        for color in Project.colors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from Colors assets")
+        }
+    }
+
+    func testJSONLoadsCorrectly() {
+        XCTAssertFalse(Award.allAwards.isEmpty, "Failed to load awards from JSON")
+    }
+}
